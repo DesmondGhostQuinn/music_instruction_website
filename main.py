db5d1dc6900faf99b53d9facb22e81f38a586b36
DesmondGhostQuinn <DJ_quinn74@yahoo.com>
2018-09-05 11:28:02 -0700

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from flask_pymongo import PyMongo
 from flask_pymongo import MongoClient
 #from flask.ext.pymongo import PyMongo
 import bcrypt
+import smtplib
+import yagmail
 
 app = Flask(__name__)
 
@@ -18,19 +20,11 @@ collection = db['users'] #just added
 
 mongo = PyMongo(app)
 
-
 @app.route('/')
 
-#def homepage():                
-    #render_template('index.html')                #just added
 
 def index():
 	if 'username' in session:
-		#return 'You are logged in as ' + session['username']
-		#return render_template('profile.html',username= session['username'])      
-		#return render_template('profile2.html',password="password")
-		#password=db.users.find({},{"password":1})
-		#password = mongo.db.users.find_one_or_404({"password": True})
 		password = mongo.db.users.find_one({'name': session['username']})
 		return render_template('profile2.html',username=password['name'],password=password['password'],instrument=password['instrument'], coursed=password['coursed'], course=password['course'])		#just added
 
@@ -41,23 +35,36 @@ def index():
 
 @app.route('/login', methods=['POST', 'GET'])
 def login():
-    #return redirect(url_for('login1'))  
-    #return render_template('login.html')          
-    if request.method == 'POST':
-        users = mongo.db.users
-        login_user = users.find_one({'name' : request.form['username']})
+	          
+	if request.method == 'POST':
+		users = mongo.db.users
+		login_user = users.find_one({'name' : request.form['username']})
 
-        if login_user:
-            #if (request.form['pass'], login_user['password']) == login_user['password']:
-            if bcrypt.hashpw(request.form['pass'].encode('utf-8'), login_user['password'].encode('utf-8')) == login_user['password'].encode('utf-8'):
-              
-                session['username'] = request.form['username']
-                return redirect(url_for('index'))        
-                #return render_template('homepage.html')
+		if login_user:
+			#if (request.form['pass'], login_user['password']) == login_user['password']:
+			if bcrypt.hashpw(request.form['pass'].encode('utf-8'), login_user['password'].encode('utf-8')) == login_user['password'].encode('utf-8'):
+				session['username'] = request.form['username']
+				session['logged_in'] = True
+				return redirect(url_for('index'))     
 
-    
-        return 'Invalid username/password combination'
-    return render_template('login.html')
+				
+
+
+		return 'Invalid username/password combination'
+	return render_template('login.html')
+	
+	
+@app.route('/logout')
+#@login_required
+def logout():
+    session.pop('logged_in', None)
+    #flash('You were logged out.')
+    return redirect(url_for('home'))	
+
+@app.route('/home')	
+def home():
+	return render_template('homepage.html')
+	
 
 @app.route('/signup', methods=['POST', 'GET'])
 def signup():
@@ -76,9 +83,75 @@ def signup():
 
     return render_template('signup.html')
 
+	
+@app.route("/form", methods=["GET", "POST"])
+def my_form():
+	if request.method == 'POST':
+		reply_to = request.form.get('email')
+		message = request.form.get('message')
+		message = reply_to+'--says that--'+message
+		yag = yagmail.SMTP('ourprojectmail24@gmail.com', 'sainasimran1')	#to initialize
+		yagmail.SMTP('ourprojectmail24@gmail.com').send('ourprojectmail24@gmail.com', reply_to, message)
+		return redirect(url_for('index'))
+		
+	return render_template('contact.html')	
+	
 @app.route('/aboutpage', methods=['GET'])
 def aboutpage():
-    return render_template('aboutpage.html')
+    return render_template('homepage.html')
+	
+@app.route('/faq')
+def faq():
+	return render_template('faq.html')
+	
+@app.route('/teachers')
+def teachers():
+	return render_template('teachers.html')
+	
+@app.route('/ccourse')
+def ccourse():
+	return render_template('ccourse.html')
+	
+@app.route('/lcourse')
+def lcourse():
+	return render_template('lcourse.html')
+	
+@app.route('/abrsm')
+def abrsm():
+	return render_template('abrsm.html')
+	
+@app.route('/lcm')
+def lcm():
+	return render_template('lcm.html')
+	
+@app.route('/trinity')
+def trinity():
+	return render_template('trinity.html')
+	
+@app.route('/bass1')
+def bass1():
+	return render_template('bass1.html')
+	
+@app.route('/guitar1')
+def guitar1():
+	return render_template('guitar1.html')
+	
+@app.route('/piano1')
+def piano1():
+	return render_template('piano1.html')
+	
+@app.route('/sax1')
+def sax1():
+	return render_template('sax1.html')
+	
+@app.route('/violin1')
+def violin1():
+	return render_template('violin1.html')
+	
+@app.route('/drums1')
+def drums1():
+	return render_template('drums1.html')
+	
 
 if __name__ == '__main__':
     app.secret_key = 'mysecret'
